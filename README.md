84c39624246c15158950fdee440d085494d2b0c7
andrea-bertaglia <andreabertaglia@gmail.com>
2024-03-26 16:42:13 +0100

    feat: milestone 1

--- README.md
+++ README.md
@@ -21,31 +21,41 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 ---
 
-## Svolgimento
+## Svolgimento MILESTONE 1
 
-### 1. Raccolta dati
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console
 
-- [ ] Chiedere all'utente il numero dei km da percorrere
-- [ ] Chiedere all'utente l'età del passeggero
-- [ ] Dichiarazione delle variabili con le percentuali di sconto
-- [ ] Dichiarazione della variabile sconto
-- [ ] Dichiarazione della variabile prezzo netto
-- [ ] Dichiarazione della variabile prezzo lordo
+### 1. Creazione degli input in pagina
 
-### 2. Logica
+### 2. Al click del pulsante viene eseguito il processo
 
-- [ ] Calcolo del prezzo lordo
-      Prezzo lordo = 0.21 x km inseriti
-- [ ] Controllo del tipo di sconto da applicare
-    Se (l'età inserita è < 18)
-    calcolo sconto = prezzo lordo x 20 / 100
-    Altrimenti se (l'età inserita è > 65)
-    calcolo sconto = prezzo lordo x 40 / 100
-<!-- Altrimenti
-    calcolo sconto = prezzo lordo * 0 / 100 -->
-- [ ] Calcolo del prezzo netto (prezzo lordo - sconto)
+1. Raccolta dati
 
-### 3. Output
+   - [x] Chiedere all'utente il numero dei km da percorrere
+   - [x] Chiedere all'utente l'età del passeggero
+   - [x] Dichiarazione delle variabili con le percentuali di sconto
+   - [x] Dichiarazione della variabile sconto
+   - [x] Dichiarazione della variabile prezzo netto
+   - [x] Dichiarazione della variabile prezzo lordo
 
-- [ ] Preparazione del messaggio con il totale (trasformato in formato con 2 decimali)
-- [ ] Stampa in pagina del totale
+2. Logica
+
+   - [x] Calcolo del prezzo lordo
+         Prezzo lordo = 0.21 x km inseriti
+   - [x] Controllo del tipo di sconto da applicare
+       Se (l'età inserita è < 18)
+       calcolo sconto = prezzo lordo x 20 / 100
+       Altrimenti se (l'età inserita è > 65)
+       calcolo sconto = prezzo lordo x 40 / 100
+   <!-- Altrimenti
+       calcolo sconto = prezzo lordo * 0 / 100 -->
+   - [x] Calcolo del prezzo netto (prezzo lordo - sconto)
+
+3. Output
+   - [x] Preparazione del messaggio con il totale (trasformato in formato con 2 decimali)
+   - [x] Stampa in console del totale
+
+## Svolgimento MILESTONE 2
+
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
--- js/script.js
+++ js/script.js
@@ -1,47 +1,51 @@
 // Test link
 console.log("Hello World!");
 
-// 1. Raccolta dati
-// Richiesta del numero di chilometri e relativa conversione in type number
-const userKm = parseInt(prompt("Indica il numero dei chilometri da percorrere")) // number | null
-console.log("km: ", userKm, typeof userKm);
+// 1. Rilevazione del pulsante in pagina
+const userBtnElem = document.getElementById("send-btn"); // object | null
+console.log(userBtnElem);
 
-// Richiesta dell'età del passeggero e relativa conversione in type number
-const userAge = parseInt(prompt("Indica l'età del passeggero")) // number | null
-console.log("age: ", userAge, typeof userAge);
+// 2. Aggiunta event listener al bottone
+userBtnElem.addEventListener("click", function () {
+console.log("CLICK BUTTON");
 
-// Dichiarazione delle variabili sconto, prezzo netto e prezzo lordo
-let discount = 0; // number
-let net = 0; // number
-let gross = 0; // number
-const price = 0.21; // number
-const discountUnder18 = 0.20; // number
-const discountOver65 = 0.40; // number
+    // 1. Raccolta dati
+    // Input dell'utente
+    const userKm = parseInt((document.getElementById("user-km")).value); // (string parsed to) number | null
+    console.log("input km", userKm, typeof userKm);
 
-// 2. Logica
-// Calcolo del prezzo lordo
-gross = userKm * price; // number
-console.log("gross: ", gross);
+    const userAge = parseInt((document.getElementById("user-age")).value); // (string parsed to) number | null
+    console.log("input age",userAge, typeof userAge);
 
-// Controllo del tipo di sconto da applicare
-if (userAge < 18) {
-    discount = gross * discountUnder18;
-    console.log("discount: ", discount);
-} else if (userAge >= 65) {
-    discount = gross * discountOver65;
-    console.log("discount: ", discount);
-}
+    // Dichiarazione delle variabili sconto, prezzo netto e prezzo lordo
+    let discount = 0; // number
+    let net = 0; // number
+    let gross = 0; // number
+    const price = 0.21; // number
+    const discountUnder18 = 0.20; // number
+    const discountOver65 = 0.40; // number
 
-// Calcolo del prezzo netto (prezzo lordo - sconto)
-net = gross - discount;
-console.log("net: ", net);
+    // 2. Logica
+    // Calcolo del prezzo lordo
+    gross = userKm * price; // number | null
+    console.log("gross: ", gross);
 
-// 3. Output
-// Preparazione del messaggio con il totale
-const resultMessage = `L'importo da pagare è: ${net.toFixed(2)} €` // string
-console.log(resultMessage);
+    // Controllo del tipo di sconto da applicare
+    if (userAge < 18) {
+        discount = gross * discountUnder18; // number
+        console.log("discount: ", discount);
+    } else if (userAge >= 65) {
+        discount = gross * discountOver65; // number
+        console.log("discount: ", discount);
+    }
 
-// Stampa in pagina
-document.getElementById("age").innerHTML = userAge;
-document.getElementById("km").innerHTML = userKm;
-document.getElementById("message").innerHTML = resultMessage;
+    // Calcolo del prezzo netto (prezzo lordo - sconto)
+    net = gross - discount; // number | null
+    console.log("net: ", net);
+
+    // 3. Output
+    // Preparazione del messaggio con il totale
+    const resultMessage = `L'importo da pagare è: ${net.toFixed(2)} €` // string | null
+    console.log(resultMessage);
+
+})
